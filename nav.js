d58ef5449994d1c7eecbb865cb9f8c9be7a2bedf
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-10-11 21:34:53 -0400

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,8 +1,9 @@
 const burger = document.querySelector(".hamburger")
 const menu = document.querySelector(".nav__links--container")
-const line = document.querySelector(".line")
+
 
 burger.addEventListener("click", () => {
     menu.classList.toggle("show")
-    line.classList.toggle("clicked")
+    burger.classList.toggle("clicked")
+   
 })
